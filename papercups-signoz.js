c4eecbbea4c4a8eceb97760ba88200b989b64a7c
Pranay Prateek <pranay@signoz.io>
2021-05-28 13:55:11 +0530

    adding papercups script

---
+++ papercups-signoz.js
@@ -0,0 +1,34 @@
+<script>
+window.Papercups = {
+  config: {
+    accountId: "2da0a6e1-f314-4827-a61b-3831743d395f",
+    title: "Welcome to SigNoz",
+    subtitle: "Ask us anything in the chat window below 😊",
+    primaryColor: "#ff6900",
+    greeting: "",
+    awayMessage: "",
+    newMessagePlaceholder: "Start typing...",
+    showAgentAvailability: false,
+    agentAvailableText: "We're online right now!",
+    agentUnavailableText: "We're away at the moment.",
+    requireEmailUpfront: false,
+    iconVariant: "outlined",
+    baseUrl: "https://app.papercups.io",
+    // Optionally include data about your customer here to identify them
+    // customer: {
+    //   name: __CUSTOMER__.name,
+    //   email: __CUSTOMER__.email,
+    //   external_id: __CUSTOMER__.id,
+    //   metadata: {
+    //     plan: "premium"
+    //   }
+    // }
+  },
+};
+</script>
+<script
+type="text/javascript"
+async
+defer
+src="https://app.papercups.io/widget.js"
+></script>
